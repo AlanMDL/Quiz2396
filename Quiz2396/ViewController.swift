fe9049f4949e60b04f1f653b8d98951d24fe6c15
Alan Marin De Luna <alan.marin1996@email.bakersfieldcollege.edu>
2018-07-07 22:07:54 -0700

    Fun Trivia Quiz App

--- Quiz2396/ViewController.swift
+++ Quiz2396/ViewController.swift
@@ -9,17 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //life cycle function that changes the ??? to the first question in the array of strings of questions
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        questionLabel.text = questions[currentQuestionIndex]
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // outlet variable variableName: objectType
+    // IBOutlet - Interface Builder Outlet, we are going to be connect these outlets to label by using IB.
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
+    
+    let questions: [String] = [
+        "What is 7+7?",
+        "What is the capital of Vermont?",
+        "What is cognac made from?"
+    ]
+    
+    let answers: [String] = [
+        "14",
+        "Montpelier",
+        "Grapes"
+    ]
+    
+    var currentQuestionIndex: Int = 0
+    
+    @IBAction func showNextQuestion(_ sender: UIButton){
+        currentQuestionIndex += 1
+        
+        //if currentQuestion is the last question in the array of Questions, then reset currentQuestionIndex to 0
+        if(currentQuestionIndex == questions.count) {
+            currentQuestionIndex = 0
+        }
+        
+        let question: String = questions[currentQuestionIndex]
+        questionLabel.text = question
+        answerLabel.text = "???"
     }
-
+    
+    @IBAction func showAnswer(_ sender: UIButton){
+        let answer: String = answers[currentQuestionIndex]
+        answerLabel.text = answer
+    }
 
 }
-
